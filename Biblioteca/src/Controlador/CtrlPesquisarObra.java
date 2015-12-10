f6c871215b59d31ff300a7d5aa9e2b8a26d46949
MAXWELL SCHNER <ms10@inf.ufpr.br>
2015-12-10 17:30:32 -0200

    Alterações no pesquisar obra

--- Biblioteca/src/Controlador/CtrlPesquisarObra.java
+++ Biblioteca/src/Controlador/CtrlPesquisarObra.java
@@ -9,54 +9,55 @@ import Modelo.Obra;
 import Modelo.Periodico;
 
 public class CtrlPesquisarObra {
-	
-	@SuppressWarnings("null")
-	public List<Integer> pesquisarobra(Integer tp_obra, String nome_obra, List<Obra> listaObra, List<Emprestimo> listaEmprestimo, List<Exemplar> listaExemplar)
-	{
-		List<Integer> retorno = null;
-		//Busca obra
+
+	public void pesquisarobra(int tp_obra, String nome_obra,
+			List<Obra> listaObra, List<Emprestimo> listaEmprestimo,
+			List<Exemplar> listaExemplar) {
+		// Busca obra
 		Obra obra = buscaObra(tp_obra, nome_obra, listaObra);
-		
-		//Percorre lista de exemplares para saber quantos exemplares daquela obra existem
-		Integer numExemplares = 0;
-		for(Exemplar exemplar : listaExemplar)
-		{
-			if (exemplar.getObra().getId() == obra.getId())
-			{
-				numExemplares = numExemplares + 1;
+
+		// Verifica se encontrou a obra
+		if (obra != null) {
+
+			// Percorre lista de exemplares para saber quantos exemplares
+			// daquela obra existem
+			Integer numExemplares = 0;
+			for (Exemplar exemplar : listaExemplar) {
+				if (exemplar.getObra().getId() == obra.getId()) {
+					numExemplares = numExemplares + 1;
+				}
 			}
-		}
-		retorno.add(numExemplares);
-		
-		//Percorre lista de emprestimos procurando os que estao disponiveis
-		Integer disponiveis = 0;
-		Integer emprestados = 0;
-		
-		for(Emprestimo emprestimo : listaEmprestimo)
-		{
-			if (emprestimo.getExemplar().getObra().getId() == obra.getId()
-					&& emprestimo.getDataDevolucao() == null)
-			{
-				emprestados = emprestados + 1;
+			System.out.println(numExemplares);
+
+			// Percorre lista de emprestimos procurando os que estao disponiveis
+			Integer disponiveis;
+			Integer emprestados = 0;
+
+			for (Emprestimo emprestimo : listaEmprestimo) {
+				if (emprestimo.getExemplar().getObra().getId() == obra.getId()
+						&& emprestimo.getDataDevolucao() == null) {
+					emprestados = emprestados + 1;
+				}
 			}
+			disponiveis = numExemplares - emprestados;
+		} else {
+			// Obra não encontrada
+			System.out.println("Obra não encontrada !");
 		}
-		disponiveis = numExemplares - emprestados;
-		retorno.add(disponiveis);
-		
-		return retorno;
-		
 	}
-	
+
 	public Obra buscaObra(int tp_obra, String nome_obra, List<Obra> listaObra) {
 		for (Obra obra : listaObra) {
 			if (tp_obra == 1) {
 				if (obra.getClass().equals(Livro.class)
-						&& obra.getNome() == nome_obra) { // && obra instanceof Livro
+						&& obra.getNome() == nome_obra) { // && obra instanceof
+															// Livro
 					return obra;
 				}
 			} else if (tp_obra == 2) {
 				if (obra.getClass().equals(Periodico.class)
-						&& obra.getNome() == nome_obra) { // && obra instanceof Periodico
+						&& obra.getNome() == nome_obra) { // && obra instanceof
+															// Periodico
 					return obra;
 				}
 			}
